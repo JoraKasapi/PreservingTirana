5745fabe32ee36841f096dbe4b86d127d274aa67
burnoutberni <hello@nini.su>
2018-08-17 12:54:56 +0200

    replace node stuff with shell script

---
+++ fetchdata.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wget https://umap.openstreetmap.fr/en/datalayer/608593/ -O copyright_action_day_2018.geojson
--- index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const fetch = require('node-fetch')
-
-fetch('https://umap.openstreetmap.fr/en/datalayer/608593/')
-  .then(response => response.text())
-  .then(response => fs.writeFileSync('copyright_action_day_2018.geojson', response))
-  .catch(error => console.error(error))
